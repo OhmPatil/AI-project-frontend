788b36feb19be0718e30e987f52b429a9b18c975
OhmPatil <workpatil10@gmail.com>
2023-05-12 10:23:50 +0530

    - Update styling. - Add flowbite components - Add loading spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useRef, useState } from 'react'
 import './App.css'
 import HeadlineCard from './components/HeadlineCard'
+import { TextInput, Button } from 'flowbite-react'
+import ScaleLoader from "react-spinners/ScaleLoader";
 
 function App() {
   const inputRef = useRef()
@@ -18,22 +20,27 @@ function App() {
   }
   
   return (
-    <>
-    <div className='text-3xl'>
-      <form onSubmit={(event) => fetchData(event)}>
-        <input ref={inputRef} type="text" placeholder='Enter subreddit...' className='text-black' />
-        <input type="submit" value={"submit"} />
+    <div className='w-full flex flex-col justify-center items-center'>
+    <div className='w-full max-w-[1280px] p-4 flex justify-center items-center'>
+      <form onSubmit={(event) => fetchData(event)} className='w-full flex justify-evenly items-center'>
+        <TextInput ref={inputRef} type="text" placeholder='Enter subreddit...' required={true} className='text-black font-semibold w-4/5' />
+        <Button type='submit'>Submit</Button>
       </form>
     </div>
     <div className='flex flex-col justify-center items-center gap-4 mt-4 w-full'>
-      {loading && <div>Loading...</div>}
+      {data && (
+        <div className='text-lg font-semibold'>
+          Latest {data.headlines.length} 'Hot' posts from r/{inputRef.current.value}
+        </div>
+      )}
+      <ScaleLoader color='#ff5100' loading={loading}/>
       {data && (
         data.headlines.map((headline, index) => {
           return <HeadlineCard key={index} headline={headline} score={data.results[index]}/>
         })
       )}
     </div>
-    </>
+    </div>
   )
 }
 
--- src/components/HeadlineCard.jsx
+++ src/components/HeadlineCard.jsx
@@ -1,8 +1,8 @@
 function HeadlineCard({ headline, score }) {
   return (
-    <div className="bg-black min-h-[125px] h-fit w-[90%] border-2 border-white rounded-xl flex justify-between items-center p-4 gap-2">
-      <div className="text-lg font-semibold w-2/3">{headline}</div>
-      <div>Sentiment Score: {score?.toFixed(2)}</div>
+    <div className="bg-[#ff5100] bg-opacity-40 min-h-[125px] h-fit w-[90%] max-w-[1280px] rounded-xl shadow-lg flex flex-col lg:flex-row justify-between items-center p-4 gap-2">
+      <h1 className="text-lg lg:text-xl font-semibold w-3/4 text-center">{headline}</h1>
+      <h2 className="lg:text-lg">Sentiment Score: {score?.toFixed(2)}</h2>
       <div className="text-5xl">
         {/* {score >= 2 && "😀"}
         {score <= -2 && score > -4 && "☹️"}
